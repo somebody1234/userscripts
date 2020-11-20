bd3279237baf53069a549243c6236e1125a844e5
E-Hern Lee <ehern.lee@gmail.com>
2020-11-21 01:06:57 +1000

    Update NRM.plugin.js

--- NRM.plugin.js
+++ NRM.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name NoReplyMention
- * @description Automatically sets replies (sender side) to not mention target (@ON -> @OFF)
+ * @description Automatically sets replies to not mention target
  * @author somebody
  * @authorId 153146360705712128
  * @authorLink https://github.com/somebody1234
@@ -10,7 +10,7 @@
  */
 function f(e) {
   if (e.target.getAttribute('aria-label') === 'Reply' || e.target.parentElement.getAttribute('aria-label') === 'Reply') {
-    setTimeout(() => { if (document.getElementsByClassName('da-mentionButton')[0].innerText === 'ON') { document.getElementsByClassName('da-mentionButton')[0].click(); } }, 0);
+    setTimeout(() => { const btn = document.getElementsByClassName('mentionButton-3710-W')[0]; if (btn.innerText === 'ON') { btn.click(); } }, 0);
   }
 }
 module.exports = class NoReplyMention {

c06bfe15ad49710555c2719fffbd6a39dedb314d
E-Hern Lee <ehern.lee@gmail.com>
2020-11-20 18:34:06 +1000

    Update NRM.plugin.js

--- NRM.plugin.js
+++ NRM.plugin.js
@@ -1,15 +1,19 @@
 /**
  * @name NoReplyMention
+ * @description Automatically sets replies to not mention target
+ * @author somebody
+ * @authorId 153146360705712128
+ * @authorLink https://github.com/somebody1234
  * @version 0.0.1
  * @source https://raw.githubusercontent.com/somebody1234/userscripts/master/NRM.plugin.js
  * @updateUrl https://raw.githubusercontent.com/somebody1234/userscripts/master/NRM.plugin.js
  */
-module.exports = class NoReplyMention {
-  start() { document.addEventListener('mouseup', NoReplyMention.f); }
-  stop() { document.removeEventListener('mouseup', NoReplyMention.f); }
-  static f(e) {
-    if (e.target.getAttribute('aria-label') === 'Reply' || e.target.parentElement.getAttribute('aria-label') === 'Reply') {
-      setTimeout(() => document.getElementsByClassName('da-mentionButton')[0].innerText === 'ON' && document.getElementsByClassName('da-mentionButton')[0].click(), 0);
-    }
+function f(e) {
+  if (e.target.getAttribute('aria-label') === 'Reply' || e.target.parentElement.getAttribute('aria-label') === 'Reply') {
+    setTimeout(() => { if (document.getElementsByClassName('da-mentionButton')[0].innerText === 'ON') { document.getElementsByClassName('da-mentionButton')[0].click(); } }, 0);
   }
+}
+module.exports = class NoReplyMention {
+  start() { document.addEventListener('mouseup', f); }
+  stop() { document.removeEventListener('mouseup', f); }
 }

d0a4594b4c6bd891692d5ee7d23657119c5684a9
E-Hern Lee <ehern.lee@gmail.com>
2020-11-21 17:45:46 +1000

    Update NRM.plugin.js

--- NRM.plugin.js
+++ NRM.plugin.js
@@ -8,12 +8,20 @@
  * @source https://raw.githubusercontent.com/somebody1234/userscripts/master/NRM.plugin.js
  * @updateUrl https://raw.githubusercontent.com/somebody1234/userscripts/master/NRM.plugin.js
  */
-function f(e) {
-  if (e.target.getAttribute('aria-label') === 'Reply' || e.target.parentElement.getAttribute('aria-label') === 'Reply') {
-    setTimeout(() => { const btn = document.getElementsByClassName('mentionButton-3710-W')[0]; if (btn.innerText === 'ON') { btn.click(); } }, 0);
-  }
-}
 module.exports = class NoReplyMention {
-  start() { document.addEventListener('mouseup', f); }
-  stop() { document.removeEventListener('mouseup', f); }
+  load() {
+    this.createPendingReply = BdApi.findModuleByProps('createPendingReply');
+    this.unpatch = function unpatch() {};
+  }
+  start() {
+    this.unpatch = BdApi.monkeyPatch(
+      this.createPendingReply,
+      'createPendingReply',
+      {before: data => data.methodArguments[0].shouldMention = false}
+    );
+  }
+  stop() {
+    this.unpatch();
+    this.unpatch = function unpatch() {};
+  }
 }
